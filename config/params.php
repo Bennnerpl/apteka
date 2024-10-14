a0fa3fadff16a5246173788f5c0baa08204139ea
Mihail drozhzhinov <mader12@yandex.ru>
2024-10-14 08:35:48 +0300

    Revert "first com"
    This reverts commit d6f5e869fdf5d55df16f033680b817477b38a857.

--- config/params.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'admin@example.com',
-    'adminPassword' => 'qwerty123456',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
-];
--- docker/database_dump/dump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# MySQL - схема и данные
-mysqldump -h database -u root -p"asd_123" -P 3306 --skip-comments data > /tmp/db.sql 2> /tmp/error.log
-mv -f /tmp/db.sql /sql/db.sql
-chmod 0777 /sql/db.sql
-
-echo "[$(date +%d.%m.%Y) $(date +%X)] Dump finished" >> /var/log/cron.log
